fe57bfc3e099f2492778101bd1175d3f105fff27
_whyvrafvr <jsie@trendev.fr>
2023-04-03 12:41:20 +0200

    log response status code

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,16 @@ import (
 	"golang.ngrok.com/ngrok/config"
 )
 
+type ResponseWriterWrapper struct {
+	http.ResponseWriter
+	c int
+}
+
+func (w *ResponseWriterWrapper) WriteHeader(c int) {
+	w.c = c
+	w.ResponseWriter.WriteHeader(c)
+}
+
 func main() {
 	ctx := context.Background()
 	fs := http.FileServer(http.Dir("./"))
@@ -22,8 +32,13 @@ func main() {
 	}
 	fmt.Println("ngrok ingress url: ", l.URL())
 	http.Serve(l, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("[%s] --> %s %s \n", r.RemoteAddr, r.Method, r.URL)
+
 		w.Header().Add("x-ngrok-file-server", "trendev")
-		fs.ServeHTTP(w, r)
-		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		ww := &ResponseWriterWrapper{w, http.StatusOK}
+
+		fs.ServeHTTP(ww, r)
+
+		log.Printf("[%s] <-- %d %s", r.RemoteAddr, ww.c, http.StatusText(ww.c))
 	}))
 }
