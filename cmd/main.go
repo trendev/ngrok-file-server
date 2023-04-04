efcfa3fc15f5bb1f33525f59396c026e9273aff0
_whyvrafvr <jsie@trendev.fr>
2023-04-04 15:22:32 +0200

    fixes issue #7

--- cmd/main.go
+++ cmd/main.go
@@ -6,72 +6,10 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/trendev/ngrok-file-server/pkg/colorlog"
 	"golang.ngrok.com/ngrok"
 	"golang.ngrok.com/ngrok/config"
 )
-
-const (
-	green   = "\033[97;42m"
-	white   = "\033[90;47m"
-	yellow  = "\033[90;43m"
-	red     = "\033[97;41m"
-	blue    = "\033[97;44m"
-	magenta = "\033[97;45m"
-	cyan    = "\033[97;46m"
-	reset   = "\033[0m"
-)
-
-type ResponseWriterWrapper struct {
-	http.ResponseWriter
-	c int
-}
-
-func (w *ResponseWriterWrapper) WriteHeader(c int) {
-	w.c = c
-	w.ResponseWriter.WriteHeader(c)
-}
-
-func methodColor(m string) string {
-	switch m {
-	case "GET":
-		return blue
-	case "POST":
-		return cyan
-	case "PUT":
-		return yellow
-	case "DELETE":
-		return red
-	case "PATCH":
-		return green
-	case "HEAD":
-		return magenta
-	case "OPTIONS":
-		return white
-	default:
-		return reset
-	}
-}
-
-func statusCodeColor(c int) string {
-	switch {
-	case c >= http.StatusOK && c < http.StatusMultipleChoices:
-		return green
-	case c >= http.StatusMultipleChoices && c < http.StatusBadRequest:
-		return white
-	case c >= http.StatusBadRequest && c < http.StatusInternalServerError:
-		return yellow
-	default:
-		return red
-	}
-}
-
-func logRequest(r http.Request) {
-	log.Printf("[%s] --> %s%s%s %s", r.RemoteAddr, methodColor(r.Method), r.Method, reset, r.URL)
-}
-
-func logResponse(w ResponseWriterWrapper, r *http.Request) {
-	log.Printf("[%s] <-- %s%d%s %s", r.RemoteAddr, statusCodeColor(w.c), w.c, reset, http.StatusText(w.c))
-}
 
 func main() {
 	ctx := context.Background()
@@ -85,10 +23,10 @@ func main() {
 	}
 	fmt.Println("ngrok ingress url: ", l.URL())
 	http.Serve(l, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logRequest(*r)
+		colorlog.LogRequest(*r)
 		w.Header().Add("x-ngrok-file-server", "trendev")
-		ww := &ResponseWriterWrapper{w, http.StatusOK}
+		ww := colorlog.NewResponseWriterWrapper(w, http.StatusOK)
 		fs.ServeHTTP(ww, r)
-		logResponse(*ww, r)
+		colorlog.LogResponse(*ww, r)
 	}))
 }
---
+++ pkg/colorlog/log.go
@@ -0,0 +1,73 @@
+package colorlog
+
+import (
+	"log"
+	"net/http"
+)
+
+const (
+	green   = "\033[97;42m"
+	white   = "\033[90;47m"
+	yellow  = "\033[90;43m"
+	red     = "\033[97;41m"
+	blue    = "\033[97;44m"
+	magenta = "\033[97;45m"
+	cyan    = "\033[97;46m"
+	reset   = "\033[0m"
+)
+
+type responseWriterWrapper struct {
+	http.ResponseWriter
+	c int
+}
+
+func (w *responseWriterWrapper) WriteHeader(c int) {
+	w.c = c
+	w.ResponseWriter.WriteHeader(c)
+}
+
+func NewResponseWriterWrapper(w http.ResponseWriter, c int) *responseWriterWrapper {
+	return &responseWriterWrapper{w, c}
+}
+
+func methodColor(m string) string {
+	switch m {
+	case "GET":
+		return blue
+	case "POST":
+		return cyan
+	case "PUT":
+		return yellow
+	case "DELETE":
+		return red
+	case "PATCH":
+		return green
+	case "HEAD":
+		return magenta
+	case "OPTIONS":
+		return white
+	default:
+		return reset
+	}
+}
+
+func statusCodeColor(c int) string {
+	switch {
+	case c >= http.StatusOK && c < http.StatusMultipleChoices:
+		return green
+	case c >= http.StatusMultipleChoices && c < http.StatusBadRequest:
+		return white
+	case c >= http.StatusBadRequest && c < http.StatusInternalServerError:
+		return yellow
+	default:
+		return red
+	}
+}
+
+func LogRequest(r http.Request) {
+	log.Printf("[%s] --> %s%s%s %s", r.RemoteAddr, methodColor(r.Method), r.Method, reset, r.URL)
+}
+
+func LogResponse(w responseWriterWrapper, r *http.Request) {
+	log.Printf("[%s] <-- %s%d%s %s", r.RemoteAddr, statusCodeColor(w.c), w.c, reset, http.StatusText(w.c))
+}
