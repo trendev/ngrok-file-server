3a6a9a21b2a76f9cd85c86601a52b0c68c007b60
_whyvrafvr <jsie@trendev.fr>
2023-04-04 15:58:54 +0200

    test statusCodeColor

--- pkg/colorlog/log_test.go
+++ pkg/colorlog/log_test.go
@@ -1,12 +1,70 @@
 package colorlog
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
 )
 
+func TestHttpStatusText(t *testing.T) {
+	tt := []struct {
+		s int    // status code
+		t string // text
+	}{
+		{http.StatusOK, "OK"},
+		{http.StatusAccepted, "Accepted"},
+		{http.StatusCreated, "Created"},
+		{http.StatusMovedPermanently, "Moved Permanently"},
+		{http.StatusGone, "Gone"},
+		{http.StatusBadRequest, "Bad Request"},
+		{http.StatusNotFound, "Not Found"},
+		{http.StatusUnauthorized, "Unauthorized"},
+		{http.StatusInternalServerError, "Internal Server Error"},
+		{http.StatusGatewayTimeout, "Gateway Timeout"},
+	}
+
+	for _, tc := range tt {
+		t.Run(fmt.Sprintf("%d", tc.s), func(t *testing.T) {
+			got := http.StatusText(tc.s)
+			if got != tc.t {
+				t.Errorf("http.StatusText(%d) == %s, got %s", tc.s, tc.t, got)
+				t.FailNow()
+			}
+		})
+	}
+}
+
+func TestStatusCodeColor(t *testing.T) {
+	tt := []struct {
+		s int    // status code
+		c string // color
+	}{
+		{http.StatusOK, green},
+		{http.StatusAccepted, green},
+		{http.StatusCreated, green},
+		{http.StatusMovedPermanently, white},
+		{http.StatusGone, yellow},
+		{http.StatusBadRequest, yellow},
+		{http.StatusNotFound, yellow},
+		{http.StatusUnauthorized, yellow},
+		{http.StatusInternalServerError, red},
+		{http.StatusGatewayTimeout, red},
+	}
+
+	for _, tc := range tt {
+		t.Run(fmt.Sprintf("%d", tc.s), func(t *testing.T) {
+			got := statusCodeColor(tc.s)
+			if got != tc.c {
+				t.Errorf("statusCodeColor(%d) == %s, got %s", tc.s, tc.c, got)
+				t.FailNow()
+			}
+		})
+	}
+}
+
 func TestMethodColor(t *testing.T) {
 	tt := []struct {
-		m, c string
+		m, c string // method and color
 	}{
 		{"GET", blue},
 		{"POST", cyan},
