2cf39ee6d5ec58a0399cf4f81f465d11ec8bfe57
_whyvrafvr <jsie@trendev.fr>
2025-06-27 14:12:04 +0200

    start upgrade to NGROK V2

--- api/main.go
+++ api/main.go
@@ -2,52 +2,61 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/trendev/ngrok-file-server/pkg/colorlog"
-	"golang.ngrok.com/ngrok"
-	"golang.ngrok.com/ngrok/config"
+	"golang.ngrok.com/ngrok/v2"
 )
 
-func setConfigHTTPEndpoint() config.Tunnel {
-	sd := flag.String("static_domain", "", "ngrok static domain")
-	p := flag.String("provider", "", "oauth2 provider")
-	o2d := flag.String("oauth2_domain", "", "oauth2 authorized oauth2_domain")
-	flag.Parse()
+func setConfigHTTPEndpoint() []ngrok.EndpointOption {
+	// sd := flag.String("static_domain", "", "ngrok static domain")
+	// p := flag.String("provider", "", "oauth2 provider")
+	// o2d := flag.String("oauth2_domain", "", "oauth2 authorized oauth2_domain")
+	// flag.Parse()
+	var opts []ngrok.EndpointOption
+	tp := `
+on_http_response:
+  - actions:
+      - type: add-headers
+        config:
+          headers:
+            x-ngrok-file-server: "trendev.fr"
+            x-endpoint-id: ${endpoint.id}
+            x-client-ip: ${conn.client_ip}
+            x-client-conn-start: ${conn.ts.start}
+            x-client-loc: ${conn.geo.country}
+            x-client-path: ${req.url.path}
+`
 
-	var opts []config.HTTPEndpointOption
-
-	if *sd != "" {
-		opts = append(opts, config.WithDomain(*sd))
-	}
-	if *p != "" {
-		if *o2d != "" {
-			opts = append(opts, config.WithOAuth(*p, config.WithAllowOAuthDomain(*o2d)),
-				config.WithRequestHeader("email", "${.oauth.user.email}"))
-		} else {
-			opts = append(opts, config.WithOAuth(*p), config.WithRequestHeader("email", "${.oauth.user.email}"))
-		}
-	}
-	return config.HTTPEndpoint(opts...)
+	// if *sd != "" {
+	// 	opts = append(opts, config.WithDomain(*sd))
+	// }
+	// if *p != "" {
+	// 	if *o2d != "" {
+	// 		opts = append(opts, config.WithOAuth(*p, config.WithAllowOAuthDomain(*o2d)),
+	// 			config.WithRequestHeader("email", "${.oauth.user.email}"))
+	// 	} else {
+	// 		opts = append(opts, config.WithOAuth(*p), config.WithRequestHeader("email", "${.oauth.user.email}"))
+	// 	}
+	// }
+	opts = append(opts, ngrok.WithTrafficPolicy(tp))
+	return opts
 }
 
 func main() {
 	fs := http.FileServer(http.Dir("./shared"))
 
-	l, err := ngrok.Listen(context.Background(),
-		setConfigHTTPEndpoint(),
-		ngrok.WithAuthtokenFromEnv(),
-	)
+	l, err := ngrok.Listen(context.Background(), setConfigHTTPEndpoint()...)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("ngrok ingress url: ", l.URL())
 	http.Serve(l, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		colorlog.LogRequest(*r)
-		w.Header().Add("x-ngrok-file-server", "trendev.fr")
+		// w.Header().Add("x-ngrok-file-server", "trendev.fr")
 		ww := colorlog.NewResponseWriterWrapper(w)
 		fs.ServeHTTP(ww, r)
 		colorlog.LogResponse(*ww, r)
