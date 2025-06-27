ab7b7a83dad01d2bb2e9c8ae30481c35c13e4ce3
_whyvrafvr <jsie@trendev.fr>
2025-06-27 14:46:21 +0200

    add google as default oauth2 provider (test)

--- api/main.go
+++ api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,25 +11,53 @@ import (
 	"golang.ngrok.com/ngrok/v2"
 )
 
+const config = `
+{
+  "on_http_response": [
+    {
+      "actions": [
+        {
+          "type": "add-headers",
+          "config": {
+            "headers": {
+              "x-ngrok-file-server": "trendev.fr",
+              "x-endpoint-id": "${endpoint.id}",
+              "x-client-ip": "${conn.client_ip}",
+              "x-client-conn-start": "${conn.ts.start}",
+              "x-client-loc": "${conn.geo.country}",
+              "x-client-path": "${req.url.path}"
+            }
+          }
+        }
+      ]
+    }
+  ],
+  "on_http_request": [
+    {
+      "actions": [
+        {
+          "type": "oauth",
+          "config": {
+            "provider": "google"
+          }
+        }
+      ]
+    }
+  ]
+}`
+
 func setConfigHTTPEndpoint() []ngrok.EndpointOption {
 	// sd := flag.String("static_domain", "", "ngrok static domain")
 	// p := flag.String("provider", "", "oauth2 provider")
 	// o2d := flag.String("oauth2_domain", "", "oauth2 authorized oauth2_domain")
 	// flag.Parse()
 	var opts []ngrok.EndpointOption
-	tp := `
-on_http_response:
-  - actions:
-      - type: add-headers
-        config:
-          headers:
-            x-ngrok-file-server: "trendev.fr"
-            x-endpoint-id: ${endpoint.id}
-            x-client-ip: ${conn.client_ip}
-            x-client-conn-start: ${conn.ts.start}
-            x-client-loc: ${conn.geo.country}
-            x-client-path: ${req.url.path}
-`
+	var c map[string]interface{}
+
+	err := json.Unmarshal([]byte(config), &c)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// if *sd != "" {
 	// 	opts = append(opts, config.WithDomain(*sd))
@@ -41,7 +70,11 @@ on_http_response:
 	// 		opts = append(opts, config.WithOAuth(*p), config.WithRequestHeader("email", "${.oauth.user.email}"))
 	// 	}
 	// }
-	opts = append(opts, ngrok.WithTrafficPolicy(tp))
+	tp, err := json.Marshal(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	opts = append(opts, ngrok.WithTrafficPolicy(string(tp)))
 	return opts
 }
 
